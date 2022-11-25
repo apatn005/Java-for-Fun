8700ad0491778f98f6eabb12cb96ab214baf6509
apatn005 <106796456+apatn005@users.noreply.github.com>
2022-11-25 07:56:11 -0800

    Create main.java

---
+++ Employee Records/main.java
@@ -0,0 +1,11 @@
+class pers_main
+{
+    public static void main()
+    {
+        
+        Retire ob2= new Retire("ram",4321,1234,33000,15);
+        ob2.provident();
+        ob2.gratuity();
+        ob2.print();
+    }
+}
